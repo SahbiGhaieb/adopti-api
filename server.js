76be39d6a16e2905906585a6873fda28e17ce3a0
mohamed sahbi ghaieb <mohamedsahbi.ghaieb@esprit.tn>
2019-04-10 20:19:18 +0100

    deploy on herocu

--- server.js
+++ server.js
@@ -1,145 +1,123 @@
-const Sequelize = require('sequelize')
-const express = require('express')
-const app = express()
-const port = 3000
-const path = require('path')
-
-
+const Sequelize = require("sequelize");
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
 
 //multer for files/images
-const multer = require('multer')
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
-
-
+const multer = require("multer");
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads/')
+  destination: function(req, file, cb) {
+    cb(null, "./uploads/");
   },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname)
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
   }
-})
+});
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
-    cb(null, true)
+  if (file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
+    cb(null, true);
   } else {
-    cb(null, false)
+    cb(null, false);
   }
-}
-const upload = multer({ storage: storage })
-
-
+};
+const upload = multer({ storage: storage });
 
 //JWT AUTH
-const jwt = require('jsonwebtoken')
-
+const jwt = require("jsonwebtoken");
 
 //MODELS
-var db = require('./models')
-const petModel = require('./models/pet')
-const userModel = require('./models/user')
+var db = require("./models");
+const petModel = require("./models/pet");
+const userModel = require("./models/user");
 
-
-
-
-const Pet = petModel(db.sequelize, Sequelize)
-const User = userModel(db.sequelize, Sequelize)
-User.belongsToMany(User, { as: "Follower", foreignKey: "FollowerId", through: "Follower_Followeds" })
-User.belongsToMany(User, { as: "Followed", foreignKey: "FollowedId", through: "Follower_Followeds" })
-
+const Pet = petModel(db.sequelize, Sequelize);
+const User = userModel(db.sequelize, Sequelize);
+User.belongsToMany(User, {
+  as: "Follower",
+  foreignKey: "FollowerId",
+  through: "Follower_Followeds"
+});
+User.belongsToMany(User, {
+  as: "Followed",
+  foreignKey: "FollowedId",
+  through: "Follower_Followeds"
+});
 
 Pet.belongsTo(User);
 
-
-
-
 //connection
-db.sequelize.sync({ alter: true }).then(function () {
-  app.listen(port, function () {
-    console.log('listening to port ' + port)
-  })
-})
+db.sequelize.sync({ alter: true }).then(function() {
+  app.listen(port, function() {
+    console.log("listening to port " + port);
+  });
+});
 
-
-
-
-
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-
-
-
-//follower_Following
-app.post('/follow', verifyToken, (req, res) => {
-  jwt.verify(req.token, 'secretkey', (err, authData) => {
+//follow
+app.post("/follow/:id", verifyToken, function(req, res) {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
     if (err) {
       res.sendStatus(403);
     } else {
-      var userOnline = jwt.decode(req.token, 'secretkey').user
-      User.findByPk(req.body.id).then(user =>
-        //res.send(user)
-        user.addFollowed(userOnline.id).then(res.send('followed'))
-      )
-    }
-  })
-})
-//Unfollowed
-app.post('/unfollow', verifyToken, (req, res) => {
-  jwt.verify(req.token, 'secretkey', (err, authData) => {
+var id = req.params.id;
+  User.findByPk(req.body.id).then(user =>
+    //res.send(user)
+    user.addFollowed(id).then(res.send("followed"))
+  );
+
+}
+});
+});
+//Unfollow
+app.post("/unfollow/:id", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
     if (err) {
       res.sendStatus(403);
     } else {
-      var userOnline = jwt.decode(req.token, 'secretkey').user
-      User.findByPk(req.body.id).then(user =>
-        //res.send(user)
-        user.removeFollowed(userOnline.id).then(res.send('unfollowed'))
-      )
-    }
-  })
-})
+var id = req.params.id;
+
+  User.findByPk(req.body.id).then(user =>
+    //res.send(user)
+    user.removeFollowed(id).then(res.send("unfollowed"))
+  );
+}
+});
+});
+
+//showFollower
+app.post("/showFollowers", verifyToken, function(req, res) {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+  User.findByPk(req.body.id).then(user =>
+    user.getFollowed().then(followers => res.send(followers))
+  );
+}
+
+});
+});
 
 //showFollowing
-app.post('/showFollowing', verifyToken, (req, res) => {
-  jwt.verify(req.token, 'secretkey', (err, authData) => {
+app.post("/showFollowing", verifyToken, function(req, res) {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
     if (err) {
       res.sendStatus(403);
     } else {
-      var userOnline = jwt.decode(req.token, 'secretkey').user
-      User.findByPk(req.body.id).then(user =>
-        user.getFollowed().then(followers =>
-          res.send(followers)
-        )
-      )
-    }
-  })
-})
-//showFollowers
-app.post('/showFollowers', verifyToken, (req, res) => {
-  jwt.verify(req.token, 'secretkey', (err, authData) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      //console.log(req.body)
-      var userOnline = jwt.decode(req.token, 'secretkey').user
-      User.findByPk(req.body.id).then(user =>
-        user.getFollower().then(followings =>
-          res.send(followings)
-        )
-      )
-    }
-  })
-})
-
-
-
-
-
-
+  User.findByPk(req.body.id).then(user =>
+    user.getFollower().then(followings => res.send(followings))
+  );
+  }
+});
+});
 //signUp
-app.post('/signUp', upload.single('photo'), (req, res) => {
+app.post("/signUp", upload.single("photo"), (req, res) => {
   var user = User.build({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
@@ -147,30 +125,29 @@ app.post('/signUp', upload.single('photo'), (req, res) => {
     photo: req.file.originalname,
     num_tel: req.body.num_tel,
     password: req.body.password
-  })
-  user.save()
-  jwt.sign({ user }, 'secretkey', (err, token) => {
+  });
+  user.save();
+  jwt.sign({ user }, "secretkey", (err, token) => {
     res.json({
       user: user,
       token
     });
     //console.log(token)
-    console.log('user creé avec succée')
-  })
-
-})
+    console.log("user creé avec succée");
+  });
+});
 //ajouter pet
-app.post('/addpet', upload.single('petImage'), verifyToken, function (req, res) {
+app.post("/addpet", upload.single("petImage"), verifyToken, function(req, res) {
   //verify then do all that
-  jwt.verify(req.token, 'secretkey', (err, authData) => {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
     if (err) {
       res.sendStatus(403);
     } else {
-      var user = jwt.decode(req.token, 'secretkey').user
+      var user = jwt.decode(req.token, "secretkey").user;
       //console.log(user.id)
 
-      var long = parseFloat(req.body.longitude)
-      var lat = parseFloat(req.body.altitude)
+      var long = parseFloat(req.body.longitude);
+      var lat = parseFloat(req.body.altitude);
 
       var pet = Pet.create({
         name: req.body.name,
@@ -184,41 +161,38 @@ app.post('/addpet', upload.single('petImage'), verifyToken, function (req, res) 
         sexe: req.body.sexe,
         photo: req.file.filename,
         UserId: user.id
-      })
+      });
       //console.log(pet)
       //pet.save()
-      res.send('insertion avec succee')
+      res.send("insertion avec succee");
     }
-  })
-})
+  });
+});
 
 //get pets by type
 
-app.get('/getpets/:type', verifyToken, function(req, res) {
-  
+app.get("/getpets/:type", verifyToken, function(req, res) {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
+    var type = req.params.type;
 
-  jwt.verify(req.token, 'secretkey', (err, authData) => {
-    var type = req.params.type ;
-
-  Pet.findAll({
-      where: {type:type},
-      include: [{model: User}]
-  }).then(pet => res.json(pet))
-  })
-
-
-  
+    Pet.findAll({
+      where: { type: type },
+      include: [{ model: User }]
+    }).then(pet => res.json(pet));
+  });
 });
 
-
 //Update pet
-app.post('/updatepet', upload.single('petImage'), verifyToken, function (req, res) {
+app.post("/updatepet", upload.single("petImage"), verifyToken, function(
+  req,
+  res
+) {
   //verify then do all that
-  jwt.verify(req.token, 'secretkey', (err, authData) => {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
     if (err) {
       res.sendStatus(403);
     } else {
-      var user = jwt.decode(req.token, 'secretkey').user
+      var user = jwt.decode(req.token, "secretkey").user;
       //console.log(user.id)
 
       Pet.update(
@@ -234,104 +208,97 @@ app.post('/updatepet', upload.single('petImage'), verifyToken, function (req, re
           photo: req.file.filename,
           UserId: user.id
         },
-        { // Clause
-          where:
-          {
+        {
+          // Clause
+          where: {
             id: id
           }
         }
       ).then(count => {
         //res.json(user)
-        res.send('Rows updated ' + count);
+        res.send("Rows updated " + count);
       });
     }
-  })
-})
+  });
+});
 //Delete pet
-app.post('/deletepet', verifyToken, function (req, res) {
+app.post("/deletepet", verifyToken, function(req, res) {
   //verify then do all that
-  jwt.verify(req.token, 'secretkey', (err, authData) => {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
     if (err) {
       res.sendStatus(403);
     } else {
-      var user = jwt.decode(req.token, 'secretkey').user
+      var user = jwt.decode(req.token, "secretkey").user;
       Pet.destroy({
         where: {
           id: req.body.id
         }
-      })
-      res.send('PET deleted')
+      });
+      res.send("PET deleted");
     }
-  })
-})
-
+  });
+});
 
 //uploadIlmage
-app.get('/uploads/:upload', function (req, res) {
+app.get("/uploads/:upload", function(req, res) {
   file = req.params.upload;
   console.log(req.params.upload);
   var img = fs.readFileSync(__dirname + "/uploads/" + file);
-  res.writeHead(200, { 'Content-Type': 'image/png' });
-  res.end(img, 'binary');
-
+  res.writeHead(200, { "Content-Type": "image/png" });
+  res.end(img, "binary");
 });
 
-
-
-
 //show all
-app.get('/showallpets', verifyToken, function (req, res) {
-
-  jwt.verify(req.token, 'secretkey', (err, authData) => {
+app.get("/showallpets", verifyToken, function(req, res) {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
     if (err) {
       res.sendStatus(403);
     } else {
-      var user = jwt.decode(req.token, 'secretkey').user
+      var user = jwt.decode(req.token, "secretkey").user;
       Pet.findAll({
         include: [{ model: User }]
-      }).then(pets => res.json(pets))
+      }).then(pets => res.json(pets));
     }
   });
-
-})
+});
 //show my pets
-app.get('/showMyPets', verifyToken, function (req, res) {
-  jwt.verify(req.token, 'secretkey', (err, authData) => {
+app.get("/showMyPets", verifyToken, function(req, res) {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
     if (err) {
       res.sendStatus(403);
     } else {
       //here goes the protected code
       //var id = req.query.id
-      var id = jwt.decode(req.token, 'secretkey').user.id
+      var id = jwt.decode(req.token, "secretkey").user.id;
       //console.log(user.id)
-      Pet.findAll({ where: { userId: id }, include: [{ model: User }] }).then(pet => res.json(pet))
+      Pet.findAll({ where: { userId: id }, include: [{ model: User }] }).then(
+        pet => res.json(pet)
+      );
     }
   });
-
-})
+});
 //show specific pet
-app.get('/showpetbyid', verifyToken, function (req, res) {
-  jwt.verify(req.token, 'secretkey', (err, authData) => {
+app.get("/showpetbyid", verifyToken, function(req, res) {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
     if (err) {
       res.sendStatus(403);
     } else {
       //here goes the protected code
-      var id = req.query.id
-      Pet.findById(id).then(pet => res.json(pet), console.log(id))
+      var id = req.query.id;
+      Pet.findById(id).then(pet => res.json(pet), console.log(id));
     }
   });
-})
-
+});
 
 //update Profile
-app.post('/update', verifyToken, upload.single('photo'), function (req, res) {
-  jwt.verify(req.token, 'secretkey', (err, authData) => {
+app.post("/update", verifyToken, upload.single("photo"), function(req, res) {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
     if (err) {
       res.sendStatus(403);
     } else {
       //console.log(req.headers.authorization)
       //here goes the protected code
-      var id = jwt.decode(req.token, 'secretkey').user.id
+      var id = jwt.decode(req.token, "secretkey").user.id;
       User.update(
         // Values to update
         {
@@ -342,31 +309,30 @@ app.post('/update', verifyToken, upload.single('photo'), function (req, res) {
           num_tel: req.body.num_tel,
           password: req.body.password
         },
-        { // Clause
-          where:
-          {
+        {
+          // Clause
+          where: {
             id: id
           }
         }
       ).then(count => {
         //res.json(user)
-        res.send('Rows updated ' + count);
+        res.send("Rows updated " + count);
       });
     }
-  })
-})
-
+  });
+});
 
 //LOGIN
-app.post('/login', function (req, res) {
-  console.log(req.body.email)
+app.post("/login", function(req, res) {
+  console.log(req.body.email);
   User.findOne({
     where: {
       email: req.body.email,
       password: req.body.password
     }
   }).then(user =>
-    jwt.sign({ user }, 'secretkey', (err, token) => {
+    jwt.sign({ user }, "secretkey", (err, token) => {
       res.json({
         user: user,
         token
@@ -374,21 +340,19 @@ app.post('/login', function (req, res) {
       console.log({
         user: user,
         token
-      })
-    }))
-
-
-})
-
+      });
+    })
+  );
+});
 
 //Verify the JWT before any protected route
 function verifyToken(req, res, next) {
   // Get auth header value
-  const bearerHeader = req.headers['authorization'];
+  const bearerHeader = req.headers["authorization"];
   // Check if bearer is undefined
-  if (typeof bearerHeader !== 'undefined') {
+  if (typeof bearerHeader !== "undefined") {
     // Split at the space
-    const bearer = bearerHeader.split(' ');
+    const bearer = bearerHeader.split(" ");
     // Get token from array
     const bearerToken = bearer[1];
     // Set the token
@@ -399,11 +363,10 @@ function verifyToken(req, res, next) {
     // Forbidden
     res.sendStatus(403);
   }
-
 }
 
 //PETS FILTERS
-app.get('/getpet/:id', function (req, res) {
+app.get("/getpet/:id", function(req, res) {
   // let sql = `SELECT * FROM pets WHERE id = ${req.params.id}`;
   // let query = dbi.query(sql, (err, result) => {
   //     if(err) throw err;
@@ -416,10 +379,10 @@ app.get('/getpet/:id', function (req, res) {
   Pet.findOne({
     where: { id: id },
     include: [{ model: User }]
-  }).then(pet => res.json(pet))
+  }).then(pet => res.json(pet));
 });
 
-app.get('/getpets/:type', function (req, res) {
+app.get("/getpets/:type", function(req, res) {
   // let sql = `SELECT * FROM pets WHERE id = ${req.params.id}`;
   // let query = dbi.query(sql, (err, result) => {
   //     if(err) throw err;
@@ -432,7 +395,7 @@ app.get('/getpets/:type', function (req, res) {
   Pet.findAll({
     where: { type: type },
     include: [{ model: User }]
-  }).then(pet => res.json(pet))
+  }).then(pet => res.json(pet));
 });
 
 // app.get('/getpets/:type', function (req, res) {
@@ -451,25 +414,31 @@ app.get('/getpets/:type', function (req, res) {
 //   }).then(pet => res.json(pet))
 // });
 
-
-app.get('/getPetsByUser/:UserId', function (req, res) {
-
+app.get("/getPetsByUser/:UserId", function(req, res) {
   var UserId = req.params.UserId;
 
   Pet.findAll({
     where: { UserId: UserId },
     include: [{ model: User }]
-  }).then(pet => res.json(pet))
+  }).then(pet => res.json(pet));
 });
 
 //END PETS FILTERS
+app.get("/", function(req, res) {
+  res.json({ title: "my route test branch sahbi" });
+});
 
+/////show all users
+app.get("/showallusers", function(req, res) {
+	
+  User.findAll().then(users => res.json(users));
+});
 
+app.get('/getuser/:id', function(req, res) {
+   
 
+    var id = req.params.id ;
 
-
-app.get('/', function (req, res) {
-  res.json({ 'title': 'my route test branch sahbi' })
-})
-
+    User.findById(id).then(pet => res.json(pet))
+});
 
